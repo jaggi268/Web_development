8a18d01365fddc365d825593a2621a330e3feb90
Jagadeesh Thottempudi <jagadeeshthottempudi@gmail.com>
2021-08-03 20:44:43 +0530

    updated

---
+++ api/Newsletter-Signup/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const request = require("request");
+
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/signup.html")
+})
+app.post("/",function(req,res)
+{
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName , lastName , email);
+})
+app.listen(3000,function()
+{
+    console.log("server is running on port 3000...");
+})
